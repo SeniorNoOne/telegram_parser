b0b4c100fcd4acdc037a3fbe55ac687d23bfd822
Martyniuk Vadym <martynyukvadum@gmail.com>
2023-07-27 23:34:33 +0300

    Bot rework
    Now code of Bot is written in class based style
    Added bot UI config file
    Added flake8 linter

--- bot.py
+++ bot.py
@@ -1,60 +1,173 @@
-import configparser
+import logging
+import time
 
+import requests
+import telebot
 from telebot import TeleBot, types
 
-
-config = configparser.ConfigParser()
-config.read("config/config.ini")
-
-bot_token = config['Telegram']['bot_token']
-bot = TeleBot(token=bot_token)
+from ui_elements import UIElements
 
 
-@bot.message_handler(commands=['start'])
-def url(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton('Set target words')
-    btn2 = types.KeyboardButton('Start parsing')
-    btn3 = types.KeyboardButton('Stop parsing')
-    btn4 = types.KeyboardButton('Delete private info')
-    markup.add(btn1, btn2, btn3, btn4)
-    bot.send_message(message.from_user.id, 'Choose an option', reply_markup=markup)
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 
-@bot.message_handler(func=lambda message: message.text == 'Set target words')
-def add_target(message):
-    response = 'Enter target words to parse (comma separated)'
-    bot.send_message(message.chat.id, response)
-    bot.register_next_step_handler(message, process_target_input)
+telebot.apihelper.ENABLE_MIDDLEWARE = True
 
 
-@bot.message_handler(func=lambda message: message.text == 'Delete private info')
-def start_parsing(message):
-    bot.send_message(message.chat.id, 'Started parsing')
-    pass
+def log_function_call(func):
+    def wrapper(*args, **kwargs):
+        user = args[1].from_user
+
+        logger.info(f"Calling function: {func.__name__}, user: {user.id}")
+        result = func(*args, **kwargs)
+        logger.info(f"Function {func.__name__} returned: {result}")
+        return result
+    return wrapper
 
 
-@bot.message_handler(func=lambda message: message.text == 'Delete private info')
-def stop_parsing(message):
-    bot.send_message(message.chat.id, 'Stopped parsing')
-    pass
+def get_bot(token):
+    tg_bot = Bot(token=token)
+    return tg_bot
 
 
-@bot.message_handler(func=lambda message: message.text == 'Delete private info')
-def delete_private_info(message):
-    bot.send_message(message.chat.id, 'Private info deleted!')
+class Bot(TeleBot):
+    ui_elements = UIElements
+    
+    def __init__(self, token, *args, **kwargs):
+        self.bot = super().__init__(token, *args, **kwargs)
+        self.target_words = []
+
+        # Callback handlers
+        self.message_handler(commands=['start'])(self.start_menu)
+
+        self.message_handler(
+            func=lambda message: message.text == 'Set target words')(self.add_target)
+
+        self.message_handler(
+            func=lambda message: message.text == 'Start parsing')(self.start_parsing)
+
+        self.message_handler(
+            func=lambda message: message.text == 'Pause parsing')(self.pause_parsing)
+
+        self.message_handler(
+            func=lambda message: message.text == 'Delete parsed info')(self.delete_parsed_info)
+
+    def send_message_with_retries(self, chat_id, text, reply_markup=None,
+                                  max_retries=3, retry_delay=2):
+        retries = 0
+        while retries < max_retries:
+            try:
+                response = self.send_message(chat_id, text, reply_markup=reply_markup)
+                return response
+            except requests.exceptions.RequestException as e:
+                print(f"Request failed (Attempt {retries + 1}): {e}")
+                retries += 1
+                time.sleep(retry_delay)
+        raise Exception(f"Max retries ({max_retries}) exceeded. Request failed.")
+
+    @staticmethod
+    def create_ui(buttons):
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        buttons = [types.KeyboardButton(button_name) for button_name in buttons]
+        markup.add(*buttons)
+        return markup
+
+    @staticmethod
+    def greeting_str(user):
+        greeting_str = 'Hello, '
+        greeting_str += f'{user.first_name} ' if user.first_name else ''
+        greeting_str += f'{user.last_name}' if user.last_name else ''
+        greeting_str += f'({user.username})' if user.username else ''
+        return greeting_str
+
+    @log_function_call
+    def start_menu(self, message):
+        ui_config = self.ui_elements['start_menu']
+        markup = self.create_ui(ui_config['ui'])
+        greeting_str = self.greeting_str(message.from_user)
+        self.send_message_with_retries(
+            message.chat.id,
+            greeting_str + '\n\n' + ui_config['msg'],
+            reply_markup=markup
+        )
+
+    @log_function_call
+    def add_target(self, message):
+        ui_config = self.ui_elements['add_target']
+        markup = self.create_ui(ui_config['ui'])
+        self.send_message_with_retries(message.chat.id, ui_config['msg'], reply_markup=markup)
+        self.register_next_step_handler(message, self.process_target_input)
+
+    @log_function_call
+    def start_parsing(self, message):
+        ui_config = self.ui_elements['start_parsing']
+        if self.target_words:
+            self.send_message_with_retries(message.chat.id, ui_config['msg'])
+            return True
+        else:
+            self.send_message_with_retries(message.chat.id, ui_config['error_msg'])
+            return False
+
+    @log_function_call
+    def pause_parsing(self, message):
+        ui_config = self.ui_elements['start_parsing']
+        self.send_message_with_retries(message.chat.id, ui_config['msg'])
+
+    @log_function_call
+    def delete_parsed_info(self, message):
+        ui_config = self.ui_elements['delete_parsed_info']
+        self.send_message_with_retries(message.chat.id, ui_config['msg'])
+
+    @log_function_call
+    def process_target_input(self, message):
+        ui_config = self.ui_elements['process_target_input']
+
+        if message.text.strip() == ui_config['exit_msg']:
+            self.start_menu(message)
+            return
+
+        markup = self.create_ui(ui_config['ui'])
+
+        response = ''
+        target_words = []
+        if message.content_type != 'text':
+            response = ui_config['error_msg'] + message.content_type
+        else:
+            user_input = message.text.split(',')
+            target_words = list(dict.fromkeys(user_input))
+
+            if target_words:
+                response = ui_config['msg'] + '\n'
+                response += ', '.join(target_words)
+
+        self.send_message_with_retries(message.chat.id, response, reply_markup=markup)
+        self.register_next_step_handler(message, self.modify_target_words, target_words)
+
+    @log_function_call
+    def modify_target_words(self, message, target_words):
+        append_cond, replace_cond = self.ui_elements['process_target_input']['ui']
+        ui_config = self.ui_elements['modify_target_words']
+
+        if message.text.strip() == append_cond:
+            self.target_words.extend(target_words)
+        elif message.text.strip() == replace_cond:
+            self.target_words = target_words
+
+        response = ui_config['msg'] + '\n'
+        response += ', '.join(self.target_words)
+        self.send_message_with_retries(message.chat.id, response)
+        self.start_menu(message)
 
 
-def process_target_input(message):
-    user_input = message.text
-    target_words = [target.strip() for target in user_input.split(',')]
+if __name__ == '__main__':
+    import configparser
 
-    if target_words:
-        response = f'Target words used in parsing:\n'
-        response += " ".join(target_words)
-        bot.send_message(message.chat.id, response)
-    else:
-        bot.send_message(message.chat.id, 'Wrong input. Try again')
+    config = configparser.ConfigParser()
+    config.read("config/config.ini")
 
+    bot_token = config['Telegram']['bot_token']
+    bot = get_bot(bot_token)
 
-bot.polling(none_stop=True, interval=0)
+    bot.polling(none_stop=True, interval=0)
---
+++ ui_elements.py
@@ -0,0 +1,44 @@
+UIElements = {
+    'start_menu': {
+        'msg': 'Please, choose an option',
+        'ui': ('Set target words', 'Start parsing', 'Pause parsing', 'Delete parsed info'),
+        'error_msg': '',
+        'exit_msg': '',
+    },
+    'add_target': {
+        'msg': 'Enter target words to parse (comma separated)',
+        'ui': ('Cancel',),
+        'error_msg': '',
+        'exit_msg': '',
+    },
+    'start_parsing': {
+        'msg': 'Started parsing',
+        'ui': (),
+        'error_msg': 'Set target words before starting parsing process',
+        'exit_msg': '',
+    },
+    'pause_parsing': {
+        'msg': 'Paused parsing',
+        'ui': (),
+        'error_msg': '',
+        'exit_msg': '',
+    },
+    'delete_parsed_info': {
+        'msg': 'Parsed info has been deleted',
+        'ui': (),
+        'error_msg': '',
+        'exit_msg': '',
+    },
+    'process_target_input': {
+        'msg': 'Target words used in parsing:',
+        'ui': ('Append new words', 'Replace existing words'),
+        'error_msg': 'Sorry, I cannot process ',
+        'exit_msg': 'Cancel',
+    },
+    'modify_target_words': {
+        'msg': 'Target words were modified:',
+        'ui': ('Append new words', 'Replace existing words'),
+        'error_msg': 'Sorry, I cannot process ',
+        'exit_msg': 'Cancel',
+    }
+}
